bcd1533b971fbec0b4ad4193345195ed3516dd69
yooni <estelle0329@ewha.ac.kr>
2025-09-20 03:37:06 +0900

    week2: db mysql로 변경

---
+++ spring-boot/src/main/java/com/ceos22/spring_boot/entity/Seat.java
@@ -0,0 +1,36 @@
+package com.ceos22.spring_boot.entity;
+
+import com.ceos22.spring_boot.common.BaseEntity;
+import com.ceos22.spring_boot.entity.mapping.Categorizing;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Seat extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seat_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "screen_id", nullable = false)
+    private Screen screen;
+
+    private Integer row_num;
+    private Integer col_num;
+
+    @Column(name = "is_special")
+    private boolean isSpecial;
+
+    // todo: 스크린 id & seatid는 unique
+
+}
+
+
+
+
+
+
+

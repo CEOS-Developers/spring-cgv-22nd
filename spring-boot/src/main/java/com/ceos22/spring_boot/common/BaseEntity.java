781dec5454036b1b4ef123c0fecea40652c38df8
yooni <estelle0329@ewha.ac.kr>
2025-09-22 00:38:56 +0900

    week2: 영화관 조회 결과값 반환 안되는 오류 해결

---
+++ spring-boot/src/main/java/com/ceos22/spring_boot/common/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.ceos22.spring_boot.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    protected LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at", nullable = true)
+    protected LocalDateTime deletedAt;
+
+}

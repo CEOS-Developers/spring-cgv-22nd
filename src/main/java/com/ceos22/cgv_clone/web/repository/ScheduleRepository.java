784b3224e66090975915c3b678a8c1bbaff7301f
dldusgh318 <pololydotoly@gmail.com>
2025-09-21 20:18:31 +0900

    repository 생성

---
+++ src/main/java/com/ceos22/cgv_clone/web/repository/ScheduleRepository.java
@@ -0,0 +1,9 @@
+package com.ceos22.cgv_clone.web.repository;
+
+import com.ceos22.cgv_clone.web.domain.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+}

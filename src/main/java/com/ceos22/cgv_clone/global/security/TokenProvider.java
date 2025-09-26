48a142d21a5de9d06c4c620deecb5e07f59f7ef6
dldusgh318 <pololydotoly@gmail.com>
2025-09-26 16:16:41 +0900

    액세스 토큰 발급 및 검증 로직

---
+++ src/main/java/com/ceos22/cgv_clone/global/security/TokenProvider.java
@@ -0,0 +1,100 @@
+package com.ceos22.cgv_clone.global.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import io.jsonwebtoken.io.Decoders;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TokenProvider implements InitializingBean {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+    private Key key;
+    private final UserDetailsService userDetailsService;
+
+    @Value("${jwt.token.access-expiration-time}")
+    private long accessExpirationTime;
+
+    @Override
+    public void afterPropertiesSet(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String createAccessToken(Long id, Authentication authentication) {
+        String authorities = authentication.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.joining(","));
+
+        Date now = new Date();
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim("auth",authorities)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + accessExpirationTime))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getAccessToken(HttpServletRequest request){
+        String token = request.getHeader("Authorization");
+        if (StringUtils.hasText(token) && token.startsWith("Bearer ")) {
+            return token.substring(7);
+        }
+        return null;
+    }
+
+    public String getTokenUserId(String token){
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public Authentication getAuthentication(String token){
+        UserDetails userDetails =
+                (UserDetails)
+                        userDetailsService.loadUserByUsername(getTokenUserId(token));
+        return new UsernamePasswordAuthenticationToken(
+                userDetails, token, userDetails.getAuthorities());
+    }
+
+    public boolean validateAccessToken(String token){
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException ex) {
+            throw new IllegalArgumentException("Invalid JWT signature or token malformed: "+ ex.getMessage());
+        } catch (ExpiredJwtException ex) {
+            throw new IllegalArgumentException("Expired JWT token: "+ ex.getMessage());
+        } catch (UnsupportedJwtException ex) {
+            throw new IllegalArgumentException("Unsupported JWT token: "+ ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            throw new IllegalArgumentException("JWT token compact of null or empty: "+ ex.getMessage());
+        }
+    }
+}
